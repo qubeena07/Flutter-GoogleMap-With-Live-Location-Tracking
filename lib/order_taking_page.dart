25c560304458da39b5b9db613045fa4cbbcbe98f
Dipika Ranabhat <qubeena7@gmail.com>
2022-08-18 18:06:25 +0545

    marker in map

--- lib/order_taking_page.dart
+++ lib/order_taking_page.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location_tracking/components/constants.dart';
 
 class OrderTrackingPage extends StatefulWidget {
   const OrderTrackingPage({Key? key}) : super(key: key);
@@ -16,9 +18,23 @@ class _OrderTrackingPageState extends State<OrderTrackingPage> {
   static const LatLng sourceLocation = LatLng(27.6844128, 85.3170161);
   static const LatLng destination = LatLng(27.662000, 85.362838);
 
-  // void getPolyPoints() async{
-  //   PolylinePoints polylinePoints = Polu
-  // }
+  List<LatLng> polylineCoordinates = [];
+
+  void getPolyPoints() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        google_api_key,
+        PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
+        PointLatLng(destination.latitude, destination.longitude));
+
+    if (result.points.isNotEmpty) {
+      for (var point in result.points) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
